e42ce9616bfd232b072ac87260c732c6cf944258
rg-esprit <guembrk.raslen@esprit.tn>
2025-01-04 18:19:12 +0100

    Remove commented-out code and clean up server-side script for Organ Trafficking feature

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,7 @@
--- Server-side script for Organ Trafficking
 
--- Example event handler
 RegisterNetEvent('RG-organ-trafficking:exampleEvent')
 AddEventHandler('RG-organ-trafficking:exampleEvent', function()
     print('Example event triggered on server side')
 end)
 
--- Add your server-side logic here
+
